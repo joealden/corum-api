528207e31c5e5a8af5b20aaa521fab9a0748d36d
Joe Alden <joerobertalden@gmail.com>
2017-12-19 11:10:42 +0000

    Add automatic post removal system

--- src/post/updateVoteCountOnVoteCreation.js
+++ src/post/updateVoteCountOnVoteCreation.js
@@ -1,8 +1,12 @@
-const { fromEvent } = require('graphcool-lib')
+import { fromEvent } from 'graphcool-lib'
+
+const VOTE_COUNT_TO_DELETE_POST = -1
 
 /*
   This is a hook function that executes every time after a vote is created.
   It updates the voteCount field on the post to reflect the vote creation.
+  Also, if the new vote count is equal or less than the VOTE_COUNT_TO_DELETE_POST
+  constant, then the post will be deleted (The automated management system)
 */
 
 const postIdFromVoteQuery = `
@@ -17,6 +21,27 @@ query getPostIdFromVote($voteId: ID!) {
 const currentPostVoteCount = `
 query getCurrentPostVoteCount($postId: ID!) {
   Post(id: $postId) {
+    voteCount
+  }
+}
+`
+const getAllVoteIdsOnPost = `
+query getAllVoteIdsOnPost($postId: ID!) {
+  allVotes(filter: { post: { id: $postId }}) {
+    id
+  }
+}
+`
+const deleteVote = `
+mutation deletePost($voteId: ID!) {
+  deleteVote(id: $voteId) {
+    id
+  }
+}
+`
+const deletePost = `
+mutation deletePost($postId: ID!) {
+  deletePost(id: $postId) {
     voteCount
   }
 }
@@ -39,7 +64,18 @@ const makeRequest = async (api, query, variables) => {
   }
 }
 
-module.exports = async event => {
+const deleteAllVotesOnPost = async (api, postId) => {
+  const { allVotes } = await makeRequest(api, getAllVoteIdsOnPost, { postId })
+  const voteIdList = allVotes.map(vote => vote.id)
+
+  return await Promise.all(
+    voteIdList.map(voteId => {
+      return makeRequest(api, deleteVote, { voteId })
+    })
+  )
+}
+
+export default async event => {
   // Retrieve payload from event
   const { data } = event
   const voteId = data.id
@@ -65,7 +101,12 @@ module.exports = async event => {
       return Promise.reject('voteType is not defined')
     }
 
-    return await makeRequest(api, updatePost, { postId, newVoteCount })
+    if (newVoteCount <= VOTE_COUNT_TO_DELETE_POST) {
+      await deleteAllVotesOnPost(api, postId)
+      return await makeRequest(api, deletePost, { postId })
+    } else {
+      return await makeRequest(api, updatePost, { postId, newVoteCount })
+    }
   } catch (error) {
     return { error }
   }
--- src/post/updateVoteCountOnVoteDeletion.js
+++ src/post/updateVoteCountOnVoteDeletion.js
@@ -1,4 +1,6 @@
-const { fromEvent } = require('graphcool-lib')
+import { fromEvent } from 'graphcool-lib'
+
+const VOTE_COUNT_TO_DELETE_POST = -1
 
 /*
   This is a hook function that executes every time after a vote is deleted.
@@ -22,6 +24,27 @@ query getCurrentPostVoteCount($postId: ID!) {
   }
 }
 `
+const getAllVoteIdsOnPost = `
+query getAllVoteIdsOnPost($postId: ID!) {
+  allVotes(filter: { post: { id: $postId }}) {
+    id
+  }
+}
+`
+const deleteVote = `
+mutation deletePost($voteId: ID!) {
+  deleteVote(id: $voteId) {
+    id
+  }
+}
+`
+const deletePost = `
+mutation deletePost($postId: ID!) {
+  deletePost(id: $postId) {
+    voteCount
+  }
+}
+`
 const updatePost = `
 mutation updatePost($postId: ID!, $newVoteCount: Int!) {
   updatePost(id: $postId, voteCount: $newVoteCount) {
@@ -40,7 +63,18 @@ const makeRequest = async (api, query, variables) => {
   }
 }
 
-module.exports = async event => {
+const deleteAllVotesOnPost = async (api, postId) => {
+  const { allVotes } = await makeRequest(api, getAllVoteIdsOnPost, { postId })
+  const voteIdList = allVotes.map(vote => vote.id)
+
+  return await Promise.all(
+    voteIdList.map(voteId => {
+      return makeRequest(api, deleteVote, { voteId })
+    })
+  )
+}
+
+export default async event => {
   // Retrieve payload from event
   const { data } = event
   const voteId = data.id
@@ -66,7 +100,12 @@ module.exports = async event => {
       return Promise.reject('voteType is not defined')
     }
 
-    return await makeRequest(api, updatePost, { postId, newVoteCount })
+    if (newVoteCount <= VOTE_COUNT_TO_DELETE_POST) {
+      await deleteAllVotesOnPost(api, postId)
+      return await makeRequest(api, deletePost, { postId })
+    } else {
+      return await makeRequest(api, updatePost, { postId, newVoteCount })
+    }
   } catch (error) {
     return { error }
   }
